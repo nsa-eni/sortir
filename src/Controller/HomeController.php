e5a8e0b070596cf3e43e42bf138e24ce3eb1967d
nicolasRabiller <nicolas.rabiller2019@campus-eni.fr>
2019-08-27 15:05:13 +0200

    homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,69 @@
 
 namespace App\Controller;
 
+use App\Form\HomeType;
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET", "POST"})
      */
-    public function index()
+    public function index(EntityManagerInterface $entityManager, Request $request)
     {
+        // creating the form
+        $searchForm = $this->createForm(HomeType::class);
+        $searchForm->handleRequest($request);
+
+        if($searchForm->isSubmitted() && $searchForm->isValid()) {
+            // dispatching data from the form
+            $params = $searchForm->getData();
+            $name = $params['search'];
+            $city = $params['city'];
+            $dateStart = $params['date_start'];
+            $dateEnd = $params['date_end'];
+            $owner = $params['owner'];
+            $subscribed = $params['subscribed'];
+            $notSubscribed = $params['notSubscribed'];
+            $eventEnded = $params['eventEnded'];
+
+            $eventsFromSearch = $entityManager->getRepository(Event::class)
+                ->searchEvent($name, $city, $dateStart, $dateEnd, $owner, $eventEnded);
+
+            $user = $this->getUser();
+
+            if (!is_null($subscribed)) {
+                $entityManager->initializeObject($user->getEvents());
+
+                $events = $user->getEvents();
+            }
+
+            if (!is_null($notSubscribed)) {
+                $entityManager->initializeObject($user->getEvents());
+                $allEvents = $entityManager->getRepository('Event')
+                    ->findAll();
+                $events = $user->getEvents();
+                $eventsNotIn = [];
+                foreach($allEvents as $e) {
+                    if (!in_array($e, $events)) {
+                        array_push($eventsNotIn, $e);
+                    }
+                }
+            }
+
+            return $this->render('home/index.html.twig', [
+                'SearchForm' => $searchForm->createView(),
+                $eventsFromSearch
+            ]);
+        }
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'searchForm' => $searchForm->createView()
         ]);
     }
 }
