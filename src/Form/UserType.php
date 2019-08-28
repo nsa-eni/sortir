bc67d117e37dd06971f867a522d2ac81f3dbf420
naclebon@gmail.com <naclebon@gmail.com>
2019-08-28 15:00:05 +0200

    regex et bddTest

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,20 +4,42 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('firstname')
-            ->add('name')
-            ->add('phone')
-            ->add('email')
-            ->add('password')
+            ->add('pseudo',TextType::class)
+            ->add('firstname',TextType::class)
+            ->add('name',TextType::class)
+            ->add('phone',NumberType::class, [
+                "constraints" => [
+                    new NotBlank([
+                        "message"=>"Le champ Ville ne peut pas être vide !"
+                    ]),
+                    new Regex(["pattern"=> " \^(\d\d\s){4}(\d\d)$\ ",
+                        "message"=>"format non valide !"])
+
+                ]])
+            ->add('email',EmailType::class, [
+                "constraints" => [
+                    new NotBlank([
+                        "message"=>"Le champ email ne peut pas être vide !"
+                    ]),
+                    new Regex(["pattern"=>" /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ ",
+                        "message"=>"Code postal au format 01000 !"])
+
+                ]])
+            ->add('password',PasswordType::class)
             ->add('actif')
             ->add('administrator')
          /*  ->add('imageFilename', FileType::class, [
