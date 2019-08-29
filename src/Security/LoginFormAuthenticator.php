454c5538247ceb700c8ed7c909730f2612313831
naclebon@gmail.com <naclebon@gmail.com>
2019-08-29 09:42:27 +0200

    security update

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,8 +85,10 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
+
+         return new RedirectResponse($this->urlGenerator->generate('user_index'));
+        //Erreur Redirection de page 
     }
 
     protected function getLoginUrl()
