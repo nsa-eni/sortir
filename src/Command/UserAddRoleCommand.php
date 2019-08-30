3764aa1a7e5297889bf743a0685107bd661062cd
Andrei Gache <andrei.gache.99@gmail.com>
2019-08-30 11:38:22 +0200

    securiser l utilisation de la commande create admin pour eviter le bag d or

--- src/Command/UserAddRoleCommand.php
+++ src/Command/UserAddRoleCommand.php
@@ -10,12 +10,13 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserAddRoleCommand extends Command
 {
     protected static $defaultName = 'user:create-admin';
-
+    private static $security_password_command = '$argon2id$v=19$m=65536,t=4,p=1$cGTbje8zprx5UiqxgcDx4g$OacKG5Jzw6gfQfXRVqDqE0+ZOkQ79YrD/qYIyvUBjIw';
     /**
      * @var EntityManagerInterface $entityManager
      */
@@ -26,10 +27,17 @@ class UserAddRoleCommand extends Command
      */
     private $passwordEncoderstring;
 
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoderstring)
+    /**
+     * @var EncoderFactoryInterface $encoderFactory
+     */
+    private $encoderFactory;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoderstring, EncoderFactoryInterface $encoderFactory)
     {
         $this->entityManager = $entityManager;
         $this->passwordEncoderstring = $passwordEncoderstring;
+        $this->encoderFactory = $encoderFactory;
+
         parent::__construct(self::$defaultName);
     }
 
@@ -44,20 +52,31 @@ class UserAddRoleCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $helper = $this->getHelper('question');
-        $question1 = new Question("Enter email: ", "guest");
-        $email = $helper->ask($input, $output, $question1);
 
-        $question2 = new Question("Enter password: ", "guest");
-        $password = $helper->ask($input, $output, $question2);
+        $encoder = $this->encoderFactory->getEncoder(User::class);
 
-        if ($this->createUser($email, $password)){
-            $message1 = sprintf("Email: %s", $email);
-            $message2 = sprintf("Password: %s", $password);
-            $output->writeln($message1);
-            $output->writeln($message2);
-            $io->success('User admin has been created');
+        $question_security = new Question("Enter password command : ");
+        $question_security->setHidden(true);
+        $pass_security = $helper->ask($input, $output, $question_security);
+
+        if ($encoder->isPasswordValid(self::$security_password_command, $pass_security, null)){
+
+            $question1 = new Question("Enter email: ", "guest");
+            $email = $helper->ask($input, $output, $question1);
+
+            $question2 = new Question("Enter password: ", "guest");
+            $password = $helper->ask($input, $output, $question2);
+
+            if ($this->createUser($email, $password)){
+                $message1 = sprintf("Email: %s", $email);
+                $message2 = sprintf("Password: %s", $password);
+                $output->writeln($message1);
+                $output->writeln($message2);
+                $io->success('User admin has been created');
+            }else
+                $io->error("Something wrong!");
         }else
-            $io->error("Something wrong!");
+            $io->error("Wrong password for use this command!");
 
     }
 
