38a7b3ab2e206e285606ebb1e5bcd23a55362e61
Andrei Gache <andrei.gache.99@gmail.com>
2019-09-02 12:09:09 +0200

    update importer utilisators avec email en cvs

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
 use App\Entity\User;
 use App\Form\RegistrationByImportFileType;
 use App\Form\RegistrationFormType;
@@ -53,28 +54,60 @@ class RegistrationController extends AbstractController
      * @Route("/admin/import_file", name="import_file_and_register", methods={"GET", "POST"})
      * @return Response
      */
-    public function registerByImportingFile(Request $request)
+    public function registerByImportingFile(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $form = $this->createForm(RegistrationByImportFileType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
             $file = (object) $form->get('file')->getData();
-            $tmp_filename = $file->getBasename();
-            try{
-                $content = IOFactory::load("/tmp/$tmp_filename");
-                $data = $content->getActiveSheet()->toArray(null, true, true, true);
-            }catch (Exception $exception){
-                print $exception->getMessage();
-            }catch (\PhpOffice\PhpSpreadsheet\Exception $exception){
-                print  $exception->getMessage();
-            }
-            dump($data);die();
+            $this->parseCsvFile($file, $form->get('site')->getData(), $passwordEncoder);
         }
-
 
         return $this->render('registration/register_by_import_file.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @param Site $site
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    private function parseCsvFile(UploadedFile $file, Site $site, UserPasswordEncoderInterface $encoder)
+    {
+        $tmp_filename = $file->getBasename();
+        try{
+            $content = IOFactory::load("/tmp/$tmp_filename");
+            $data = $content->getActiveSheet()->toArray(null, true, true, true);
+            $this->createUsers($site, $data, $encoder);
+        }catch (Exception $exception){
+            print $exception->getMessage();
+        }catch (\PhpOffice\PhpSpreadsheet\Exception $exception){
+            print  $exception->getMessage();
+        }
+    }
+
+    /**
+     * @param Site $site
+     * @param array $data
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    private function createUsers(Site $site, array $data, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        foreach ($data as $datum){
+            if (is_null($userRepo->findOneBy(['email'=> $datum['A']]))){
+                $user = new User();
+                $user->setPseudo('user.'.$datum['A']);
+                $user->setEmail($datum['A']);
+                $user->setPassword($encoder->encodePassword($user, 'password'));
+                $user->setSite($site);
+                $em->persist($user);
+            }
+
+        }
+        $em->flush();
     }
 }
