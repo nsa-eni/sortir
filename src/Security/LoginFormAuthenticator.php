2393db8c1d9838d698a89b5a860a5e5f0ce5234b
SKorbean <air-corback@wanadoo.fr>
2019-09-04 16:47:36 +0200

    c'est encore mieux

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -90,7 +90,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('welcome'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
